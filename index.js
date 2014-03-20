4df633847177ba6169f7ac251a167ba59419a1ef
Zolmeister <zolikahan@gmail.com>
2014-03-20 18:26:26 +0000

    added fcall and nfcall

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ Promise.prototype.tap = function Promise$tap(didFulfill, didReject) {
     });
 };
 
+Promise.fcall = Promise['try']
+
+Promise.nfcall = function Promise$_nfcall() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var fn = args.shift();
+    return Promise.promisify(fn).apply(null, args);
+}
+
 module.exports = Promise;
--- test/test.js
+++ test/test.js
@@ -1,12 +1,40 @@
 var assert = require('assert');
 var Promise = require('../index.js');
 
-describe('Goldfinch', function() {
-    it('Taps', function() {
-        return Promise.resolve('a').tap(function(){
+describe('Goldfinch', function () {
+    it('Taps', function () {
+        return Promise.resolve('a').tap(function () {
             return 0;
-        }).then(function(a) {
+        }).then(function (a) {
             return assert.equal(a, 'a');
         });
     });
+
+    it('fcalls', function () {
+        return Promise.fcall(function (b) {
+            assert.equal(b, 'b')
+            return b;
+        }, 'b').then(function(b) {
+            assert.equal(b, 'b')
+        });
+    });
+
+    it('nfcalls', function () {
+        return Promise.nfcall(function (a, cb) {
+            cb(null, a)
+        }, 'a').then(function (a) {
+            assert.equal(a, 'a')
+        })
+    });
+
+    it('nfcalls - error', function () {
+        Promise.nfcall(function (cb) {
+            cb(new Error('expected'))
+        }).then(function (nul) {
+            throw new Error('Expected error')
+        }, function (err) {
+            assert.equal(err.message, 'expected')
+        });
+    });
+
 });
