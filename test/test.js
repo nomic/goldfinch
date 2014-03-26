958d80c7195555d2908e3f66768f85819ec8f483
Zolmeister <zolikahan@gmail.com>
2014-03-26 22:42:00 +0000

    updated mocha version for proper promise support

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var Promise = require('../index.js');
 
 describe('Goldfinch', function () {
     it('Taps', function () {
-        return Promise.resolve('a').tap(function () {
-            return 0;
+        return Promise.resolve('a').tap(function (a) {
+            return assert.equal(a, 'a');
         }).then(function (a) {
             return assert.equal(a, 'a');
         });
@@ -12,28 +12,29 @@ describe('Goldfinch', function () {
 
     it('fcalls', function () {
         return Promise.fcall(function (b) {
-            assert.equal(b, 'b')
+            assert.equal(b, 'b');
             return b;
         }, 'b').then(function(b) {
-            assert.equal(b, 'b')
+            assert.equal(b, 'b');
         });
     });
 
     it('nfcalls', function () {
         return Promise.nfcall(function (a, cb) {
-            cb(null, a)
+            assert.equal(a, 'a');
+            cb(null, a);
         }, 'a').then(function (a) {
-            assert.equal(a, 'a')
-        })
+            assert.equal(a, 'a');
+        });
     });
 
     it('nfcalls - error', function () {
-        Promise.nfcall(function (cb) {
-            cb(new Error('expected'))
+        return Promise.nfcall(function (cb) {
+            cb(new Error('expected'));
         }).then(function (nul) {
-            throw new Error('Expected error')
+            throw new Error('Expected error');
         }, function (err) {
-            assert.equal(err.message, 'expected')
+            assert.equal(err.message, 'expected');
         });
     });
 
@@ -46,16 +47,19 @@ describe('Goldfinch', function () {
                 cb('oops');
             }
         });
-        obj.good(5).then(function(val) {
+
+        return obj.good(5).then(function(val) {
             assert.equal(val, 6);
         }, function(err) {
             throw new Error('Unexpected error');
+        }).then(function() {
+            return obj.bad(5).then(function(val) {
+                throw new Error('Expected error');
+            }, function(err) {
+                assert.equal(err.message, 'oops');
+            });
         });
-        obj.bad(5).then(function(val) {
-            throw new Error('Expected error');
-        }, function(err) {
-            assert.equal(err.message, 'oops');
-        });
+
     });
 
 });
